c1323a048be8557c3f8bd4a1beda7969a2c85327
Martyn_Kalchev <petrovmartin743@gmail.com>
2025-12-11 13:22:34 +0200

    .....

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 from config import Config
 
 db = SQLAlchemy()
-migrate = Migrate()                         
+migrate = Migrate()          
 login_manager = LoginManager()
 login_manager.login_view = 'routes.login'
 login_manager.login_message_category = "info"
@@ -18,35 +18,35 @@ def create_app():
     app.config.from_object(Config)
 
     # Създаване на папка за снимки
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True) #
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
     # Инициализация
-    db.init_app(app) #
-    migrate.init_app(app, db)               
-    login_manager.init_app(app) #
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     # Импорт на модели и руутове
     from .models import User
     from .routes import bp as routes_bp
-    app.register_blueprint(routes_bp) #
+    app.register_blueprint(routes_bp)
 
     @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id)) #
+        return User.query.get(int(id))
 
     # Създаване на таблици + админ
     with app.app_context():
-        db.create_all() #
-        if not User.query.first(): #
+        db.create_all()
+        if not User.query.first():
             from werkzeug.security import generate_password_hash
             admin = User(
                 username='admin',
                 email='admin@carmarket.bg',
                 password_hash=generate_password_hash('admin123'),
-                is_admin=True # НОВО: Маркиране като администратор
+                is_admin=True # Маркиране на потребителя като админ
             )
-            db.session.add(admin) #
-            db.session.commit() #
-            print("СЪЗДАДЕН Е АДМИН: admin / admin123") #
+            db.session.add(admin) 
+            db.session.commit()
+            print("СЪЗДАДЕН Е АДМИН: admin / admin123") 
 
     return app
